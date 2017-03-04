add4df5391bcd8d17e408c879cd26a30219eb261
Jan-Marten de Boer <janmarten.deboer@mediact.nl>
2017-03-04 19:19:22 +0100

    Fix broken unit tests

--- tests/DependencyInjection/Compiler/AbstractContainerPassTest.php
+++ tests/DependencyInjection/Compiler/AbstractContainerPassTest.php
@@ -67,4 +67,49 @@ class AbstractContainerPassTest extends PHPUnit_Framework_TestCase
 
         return $pass;
     }
+
+    /**
+     * @return void
+     * @covers ::process
+     */
+    public function testMissingDefinition()
+    {
+        /** @var AbstractContainerPass|PHPUnit_Framework_MockObject_MockObject $pass */
+        $pass = $this->getMockForAbstractClass(AbstractContainerPass::class);
+
+        $pass
+            ->expects($this->once())
+            ->method('getContainerIdentifier')
+            ->willReturn('container');
+
+        $pass
+            ->expects($this->never())
+            ->method('getMethodName');
+
+        $pass
+            ->expects($this->never())
+            ->method('getTagName');
+
+        /** @var ContainerBuilder|PHPUnit_Framework_MockObject_MockObject $container */
+        $container = $this->createMock(ContainerBuilder::class);
+
+        /** @var Definition|PHPUnit_Framework_MockObject_MockObject $definition */
+        $definition = $this->createMock(Definition::class);
+
+        $container
+            ->expects($this->once())
+            ->method('getDefinition')
+            ->with('container')
+            ->willReturn($definition);
+
+        $container
+            ->expects($this->never())
+            ->method('findTaggedServiceIds');
+
+        $definition
+            ->expects($this->never())
+            ->method('addMethodCall');
+
+        $pass->process($container);
+    }
 }
